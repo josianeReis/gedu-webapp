7ab00d878bcc0da971cf23ce239528e858cf5179
Software Brasil <P1403@localhost.localdomain>
2019-07-26 10:37:20 -0300

    Create stepper base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
-import LoginTest from './LoginTest.js'
+import React, { Component } from 'react';
+import Stepper from "./components/stepper";
+import './css/app.css';
 
 class App extends Component {
   render() {
-    return <LoginTest />
+    return (
+      <div className='width-app'>
+        <Stepper />
+      </div>
+    )
   }
 }
 
