1b84643aeca17b9256e6d1acd543bd288ebce593
josianereis <josiane-reis@outlook.com>
2019-08-01 15:35:47 -0300

    alterando import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import LoginTest from './LoginTest.js'
+import React, { Component } from 'react';
+import LoginTest from './pages/LoginTest';
 
 class App extends Component {
   render() {
